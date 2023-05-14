efc651345ab874229ae68d9dc6d0553b5ccbaa6e
Iwan-name <dearivan.vasilenko@yandex.ru>
2023-05-14 18:51:38 +0300

    модели и user

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
 
+from api.views import UserViewSet
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 from rest_framework import routers
@@ -10,22 +13,7 @@ router = routers.DefaultRouter()
 router.register('Category', CategoryViewSet, basename='Category')
 router.register('Genre', GenreViewSet, basename='Genre')
 router.register('Title', TitleViewSet, basename='Title')
-
-urlpatterns = [
-    path('v1', include(router.urls))
-from django.urls import include, path
-from rest_framework import routers
-
-from api.views import UserViewSet
-
-app_name = 'api'
-router = routers.DefaultRouter()
-
 router.register(r'users', UserViewSet, basename='users')
-
-
 urlpatterns = [
-    # path('v1/auth/signup/', ),
-    # path('v1/auth/token/', )
     path('v1/', include(router.urls))
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,14 +117,14 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ],
+    'PAGE_SIZE': 1,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(days=100),
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-# }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=100),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
