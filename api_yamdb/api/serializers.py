6891df470cc2d9b5d256d2c9cf1ec2953f66a4d7
Iwan-name <dearivan.vasilenko@yandex.ru>
2023-05-14 10:40:20 +0300

    модели и user

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,66 @@
 from rest_framework import serializers
 
+from reviews.models import Category, Genre, Title
 from users.models import User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitlesGetSerializer(serializers.ModelSerializer):
+    """Сериализатор для GET-запросов к произведениям."""
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'description',
+            'category',
+            'genre',
+        )
+        model = Title
+
+
+class TitlesPostSerializer(serializers.ModelSerializer):
+    """Сериализатор для POST-запросов к произведениям."""
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+
+    class Meta:
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'description',
+            'category',
+            'genre',
+        )
+        model = Title
 
 
 class UserSerializer(serializers.ModelSerializer):
